17e39b76a5a59988862cc6db3ef2f810c9d3254c
kyleso <kyle@laterolabs.com>
2020-06-13 15:23:14 -0700

    add mst root-store, environment and root-store providers, api wrapper, camelization utils

---
+++ app/javascript/services/api.ts
@@ -0,0 +1,30 @@
+import { create, ApisauceInstance } from "apisauce";
+import { camelizeResponse, decamelizeRequest } from "../utils";
+
+export class Api {
+  client: ApisauceInstance;
+  token: string;
+
+  constructor() {
+    this.client = create({
+      baseURL: "/api",
+      headers: {
+        "Cache-Control": "no-cache",
+        "Content-Type": "application/json",
+      },
+      timeout: 30000,
+    });
+
+    this.client.addResponseTransform((response) => {
+      response.data = camelizeResponse(response.data);
+    });
+
+    this.client.addRequestTransform((request) => {
+      request.params = decamelizeRequest(request.params);
+    });
+  }
+
+  addMonitor(monitor) {
+    this.client.addMonitor(monitor);
+  }
+}
