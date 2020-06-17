cc6f4fda38622fb5bfb4324721c2738b2d95f270
Christopher Pang <christopher@laterolabs.com>
2020-06-17 12:10:32 -0700

    reverted some code

--- app/javascript/lib/with-root-store.ts
+++ app/javascript/lib/with-root-store.ts
@@ -1,10 +1,10 @@
-// import { getRoot, IStateTreeNode } from "mobx-state-tree";
-// import { IRootStore } from "../stores/root-store";
+import { getRoot, IStateTreeNode } from "mobx-state-tree";
+import { IRootStore } from "../stores/root-store";
 
-// export const withRootStore = () => (self: IStateTreeNode) => ({
-//   views: {
-//     get rootStore() {
-//       return getRoot<IRootStore>(self);
-//     },
-//   },
-// });
+export const withRootStore = () => (self: IStateTreeNode) => ({
+  views: {
+    get rootStore() {
+      return getRoot<IRootStore>(self);
+    }
+  }
+});
--- app/javascript/setup/root-store.ts
+++ app/javascript/setup/root-store.ts
@@ -1,42 +1,22 @@
-// import * as Monitors from "./api-monitors";
-// import { setupEnvironment } from "./environment";
-// import { RootStoreModel } from "../stores/root-store";
-// import { onSnapshot } from "mobx-state-tree";
+import * as Monitors from "./api-monitors";
+import { setupEnvironment } from "./environment";
+import { RootStoreModel } from "../stores/root-store";
 
 export async function setupRootStore() {
   const environment = await setupEnvironment();
-
-  console.log("setup root store");
-
   const initialState = {
-    router: environment.router,
-    userStore: {
-      users: [],
-      count: 5
-    }
+    router: environment.router
   };
 
-  //   const initialState = {
-  //     router: {},
-  //     userStore: {
-  //       users: []
-  //     },
-  //     issueStore: {
-  //       issues: []
-  //     }
-  //   };
+  const rootStore = RootStoreModel.create(initialState, environment);
 
-  //   const rootStore = RootStoreModel.create(initialState, environment);
+  // add API Monitors
+  const { api } = environment;
 
-  //   onSnapshot(rootStore, snapshot => console.log("Snapshot: ", snapshot));
+  if (process.env.NODE_ENV !== "production") {
+    console.tron.trackMstNode(rootStore);
+    Monitors.addLoggingMonitor(api);
+  }
 
-  //   // add API Monitors
-  //   const { api } = environment;
-
-  //   if (process.env.NODE_ENV !== "production") {
-  //     console.tron.trackMstNode(rootStore);
-  //     Monitors.addLoggingMonitor(api);
-  //   }
-
-  //   return rootStore;
+  return rootStore;
 }
