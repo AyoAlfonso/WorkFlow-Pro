f50735a844c034191d25ccd0fae19428cf9ad124
Christopher Pang <christopher@laterolabs.com>
2020-06-24 09:10:24 -0700

    prettier settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["prettier", "@typescript-eslint"],
   extends: [
+    "prettier",
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
