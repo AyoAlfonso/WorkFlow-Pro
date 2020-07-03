8b9b4bbbf5480cc6a205331fce757ebe9e058f8b
Sunny <sunny@laterolabs.com>
2020-07-03 15:43:06 -0700

    [ch23102] fix storybook with i18n

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,6 +1,8 @@
 import { configure, addDecorator } from "@storybook/react";
 import { themeDecorator } from "../decorators/themeDecorator";
 import StoryRouter from "storybook-react-router";
+import { withI18next } from "storybook-addon-i18next";
+import i18n from "../app/javascript/i18n/i18n";
 
 const req = require.context("../", true, /\.stories\.tsx$/);
 function loadStories() {
@@ -8,4 +10,13 @@ function loadStories() {
 }
 addDecorator(themeDecorator);
 addDecorator(StoryRouter());
+addDecorator(
+  withI18next({
+    i18n,
+    languages: {
+      en: "English",
+    },
+  }),
+);
+
 configure(loadStories, module);
