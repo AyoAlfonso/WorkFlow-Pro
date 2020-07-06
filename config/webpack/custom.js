9415db813b38716e2c874b6fb0e068481fc0af3d
Jeremy <jeremy@laterolabs.com>
2020-07-06 15:17:32 -0700

    implement root import resolver

---
+++ config/webpack/custom.js
@@ -0,0 +1,18 @@
+module.exports = {
+  resolve: {
+    alias: {
+      "~/assets": "assets",
+      "~/channels": "channels",
+      "~/components": "components",
+      "~/i18n": "i18n",
+      "~/lib": "lib",
+      "~/models": "models",
+      "~/services": "services",
+      "~/setup": "setup",
+      "~/stores": "stores",
+      "~/stylesheets": "stylesheets",
+      "~/themes": "themes",
+      "~/utils": "utils",
+    },
+  },
+};
