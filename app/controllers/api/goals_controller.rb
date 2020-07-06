ba22a041d3d8c58631942a14e34e8d3857f35008
Christopher Pang <christopher@laterolabs.com>
2020-07-06 14:41:00 -0700

    fixed syntax error

--- app/controllers/api/goals_controller.rb
+++ app/controllers/api/goals_controller.rb
@@ -6,14 +6,14 @@ class Api::GoalsController < Api::ApplicationController
     @company_goals = policy_scope(AnnualInitiative).for_users_company(current_user).as_json(root: true, include: { quarterly_goals: { include: :milestones } })
     @rallying_cry = Company.find(current_user.company_id)[:rallying_cry]
     render json: { 
-    user: {
-      personal_vision: current_user[:personal_vision]
-      goals: @user_goals
-    }, 
-    company: {
-      rallying_cry: @rallying_cry,
-      goals: @company_goals
+      user: {
+        personal_vision: current_user[:personal_vision],
+        goals: @user_goals
+      }, 
+      company: {
+        rallying_cry: @rallying_cry,
+        goals: @company_goals
+      }
     }
-  }
   end
 end
