e1958dd14998902f084fe9992236fe94f40deadc
Jeremy <jeremy@laterolabs.com>
2020-07-07 11:21:53 -0700

    install actiontext consolidate packed styles implement actiontext js and css omit print.scss because it is breaking ui

--- app/javascript/packs/active_admin.js
+++ app/javascript/packs/active_admin.js
@@ -1,7 +1,9 @@
 // Load Active Admin's styles into Webpacker,
 // see `active_admin.scss` for customization.
-import "../stylesheets/active_admin";
+import "../stylesheets/index";
 import "@activeadmin/activeadmin";
 import "arctic_admin";
 import "@fortawesome/fontawesome-free/js/all.js";
 import "@fortawesome/fontawesome-free/css/all.css";
+require("trix");
+require("@rails/actiontext");
