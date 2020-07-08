68738b0588f50da7d7bf4716216a0b2f5e30e2d8
Jeremy <jeremy@laterolabs.com>
2020-07-08 12:44:35 -0700

    [ch22732] basic specs for key activities

---
+++ spec/factories/key_activities.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :key_activity do
+    user
+    description { "Test Description la-di-da-di-da-di"}
+  end
+end
---
+++ spec/models/key_activity_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe KeyActivity, type: :model do
+  it { should belong_to(:user) }
+  it { should validate_presence_of(:description) }
+
+  context "Basic validations" do
+    let!(:key_activity) { create(:key_activity) }
+    it "priority defaults to low" do
+      expect(key_activity.priority).to eq("low")
+    end
+    it "completed at defaults to nil" do
+      expect(key_activity.completed_at).to be_nil
+    end
+  end
+end
