b92f4edfdad5ed41246152e321a644b5b05c7c88
kyleso <kyle@laterolabs.com>
2020-07-08 01:33:18 -0700

    hook up toaster to api monitor and show appropriate response

--- app/javascript/setup/api-monitors.ts
+++ app/javascript/setup/api-monitors.ts
@@ -1,32 +1,49 @@
 import * as R from "ramda";
-import { TIMEOUT_ERROR, UNKNOWN_ERROR, CONNECTION_ERROR } from "apisauce";
+import { TIMEOUT_ERROR, UNKNOWN_ERROR, CONNECTION_ERROR, CLIENT_ERROR } from "apisauce";
+import { showToast } from "../utils/toast-message";
+import { ToastMessageConstants } from "../constants/toast-types";
 
 // Adds a console.log to every request
-export const addLoggingMonitor = (api) => {
+export const addLoggingMonitor = api => {
   if (process.env.NODE_ENV !== "production" || process.env.DEBUG === "true") {
-    api.addMonitor((response) => {
-      if (
-        R.contains(response.problem, [
-          TIMEOUT_ERROR,
-          UNKNOWN_ERROR,
-          CONNECTION_ERROR,
-        ])
-      ) {
+    api.addMonitor(response => {
+      if (R.contains(response.problem, [TIMEOUT_ERROR, UNKNOWN_ERROR, CONNECTION_ERROR])) {
         console.log(`[API] ERROR: \n`, response, `\n\n`);
       } else {
-        const path = R.replace(
-          response.config.baseURL,
-          "",
-          response.config.url
-        );
+        const path = R.replace(response.config.baseURL, "", response.config.url);
         const status = response.status;
         const method = R.toUpper(response.config.method);
         console.log(
           `[API] status: ${status} | method: ${method} | path: ${path}\n`,
           response,
-          `\n\n`
+          `\n\n`,
         );
       }
     });
   }
+};
+
+export const addToastMonitor = api => {
+  api.addMonitor(response => {
+    if (response.status !== 200 || response.problem !== null) {
+      switch (response.problem) {
+        case CLIENT_ERROR:
+          showToast("An client error occurred", ToastMessageConstants.ERROR);
+          break;
+        case CONNECTION_ERROR:
+          showToast("A connection error occurred", ToastMessageConstants.ERROR);
+          break;
+        case UNKNOWN_ERROR:
+          showToast("An unknown error occurred", ToastMessageConstants.ERROR);
+          break;
+        case TIMEOUT_ERROR:
+          showToast("A timeout error occurred", ToastMessageConstants.ERROR);
+          break;
+        default:
+          showToast("Something went wrong", ToastMessageConstants.ERROR);
+      }
+    } else if (response.status === 403) {
+      showToast("An authorization error occurred", ToastMessageConstants.ERROR);
+    }
+  });
 };
