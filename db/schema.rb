7201c5700ea53914c1d881465b82b3ed6a28196b
Christopher Pang <christopher@laterolabs.com>
2020-07-09 16:30:15 -0700

    removed progress and using status for milestones

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_175429) do
+ActiveRecord::Schema.define(version: 2020_07_09_232454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -173,7 +173,6 @@ ActiveRecord::Schema.define(version: 2020_07_08_175429) do
     t.bigint "quarterly_goal_id", null: false
     t.text "description"
     t.integer "week"
-    t.integer "progress", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "status", default: 0
