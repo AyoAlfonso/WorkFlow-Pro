03710648cbe6f9e6792e9addbaf7a6e7be956865
Jeremy <jeremy@laterolabs.com>
2020-07-10 08:57:44 -0700

    fix migration for rollback

--- db/migrate/20200709185926_remove_core_four_fields.rb
+++ db/migrate/20200709185926_remove_core_four_fields.rb
@@ -1,8 +1,8 @@
 class RemoveCoreFourFields < ActiveRecord::Migration[6.0]
   def change
-    remove_column :core_fours, :core_1
-    remove_column :core_fours, :core_2
-    remove_column :core_fours, :core_3
-    remove_column :core_fours, :core_4
+    remove_column :core_fours, :core_1, :text
+    remove_column :core_fours, :core_2, :text
+    remove_column :core_fours, :core_3, :text
+    remove_column :core_fours, :core_4, :text
   end
 end
