bd7e7c369645d511ef9f910816a142a84e8f1865
Sunny <sunny@laterolabs.com>
2020-07-16 18:04:34 -0700

    [ch22869] initial local version of being able to put in rich text fields for strategic plan and accountability chart.

--- app/javascript/components/domains/company/accountability-chart.tsx
+++ app/javascript/components/domains/company/accountability-chart.tsx
@@ -1,5 +1,28 @@
 import * as React from "react";
+import { observer } from "mobx-react";
+import { useMst } from "../../../setup/root";
+import { Loading } from "../../shared/loading";
 
-export const AccountabilityChart = (): JSX.Element => {
-  return <div>AccountabilityChart</div>;
-};
+export const AccountabilityChart = observer(
+  (): JSX.Element => {
+    const {
+      companyStore: { company },
+    } = useMst();
+    return (
+      <div>
+        {company ? (
+          <>
+            <div
+              className="trix-content"
+              dangerouslySetInnerHTML={{ __html: company.accountabilityChartContent }}
+            ></div>
+            <br />
+            <div dangerouslySetInnerHTML={{ __html: company.strategicPlanContent }}></div>
+          </>
+        ) : (
+          <Loading />
+        )}
+      </div>
+    );
+  },
+);
