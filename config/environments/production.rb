8f638ef5689e91f797d9851a3d1fa3463985d129
Jeremy <jeremy@laterolabs.com>
2020-07-21 11:07:43 -0700

    [ch22800] customize base email templates with lynchpin assets add letter-opener set default devise mailer layout configure action-mailer asset host for dev and prod add font-awesome style link to mailer layout style mailer layout

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "lynchpyn_production"
 
   config.action_mailer.perform_caching = false
+  config.action_controller.asset_host = ENV["PROD_URL]"
+  config.action_mailer.asset_host = config.action_controller.asset_host
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
