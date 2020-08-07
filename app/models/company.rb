387b72278dd74f5944785d17ccddcebf405fffb4
Sunny <sunny@laterolabs.com>
2020-08-07 09:45:41 -0700

    [ch22859][ch22864] updated to use embed

--- app/models/company.rb
+++ app/models/company.rb
@@ -6,11 +6,6 @@ class Company < ApplicationRecord
   
   has_one :core_four, dependent: :destroy
   accepts_nested_attributes_for :core_four
-  has_rich_text :accountability_chart
-  has_rich_text :strategic_plan
-  
-  include RichTextHelper
-  rich_text_content_render :accountability_chart, :strategic_plan
 
   validates :name, :fiscal_year_start, :timezone, presence: true
 
@@ -22,15 +17,23 @@ class Company < ApplicationRecord
     super || build_core_four
   end
 
+  def logo_url
+    logo.present? ? Rails.application.routes.url_helpers.rails_blob_url(logo, host: ENV["ASSETS_HOST_URL"] || ENV["HOST_URL"]) : nil
+  end
+
+  def accountability_chart_content
+    accountability_chart_embed
+  end
+
+  def strategic_plan_content
+    strategic_plan_embed
+  end
+
   def format_fiscal_year_start
     return "" if fiscal_year_start.blank?
     month = sprintf('%02d', fiscal_year_start.month)
     day = sprintf('%02d', fiscal_year_start.day)
     "#{month}/#{day}"
-  end
-
-  def logo_url
-    logo.present? ? Rails.application.routes.url_helpers.rails_blob_url(logo, host: ENV["ASSETS_HOST_URL"] || ENV["HOST_URL"]) : nil
   end
 
   def format_month_and_day(date)
