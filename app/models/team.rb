3b8460412d58baa525a5ac55ab24105a394759cf
Christopher Pang <christopher@laterolabs.com>
2020-08-24 16:45:50 -0700

    logic to determine average score for team

--- app/models/team.rb
+++ app/models/team.rb
@@ -23,4 +23,20 @@ class Team < ApplicationRecord
     true
   end
 
+  def weekly_average_users_emotion_score
+    q_attempts = QuestionnaireAttempt.where(user: self.users)
+                                      .where("emotion_score IS NOT NULL AND completed_at <= ? AND completed_at >= ?", 1.day.ago, 1.week.ago)
+                                      .select(:id, :completed_at, :emotion_score)
+                                      .group_by{|qa| qa.completed_at}
+    results_array = []
+    q_attempts.map do |qa|
+      average_score_hash = {
+        date: qa[0].to_date,
+        average_score: qa[1].pluck(:emotion_score).inject(:+).to_f / qa[1].size
+      }
+      results_array << average_score_hash
+    end
+    results_array
+  end
+
 end
