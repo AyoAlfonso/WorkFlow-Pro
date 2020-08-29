958c859a3038c625b1119113796f4540e6c30960
Christopher Pang <christopher@laterolabs.com>
2020-08-29 02:58:21 -0700

    Step process bar fix

--- app/javascript/components/domains/meetings/shared/progress-transform-helper.ts
+++ app/javascript/components/domains/meetings/shared/progress-transform-helper.ts
@@ -1,10 +1,16 @@
 import * as R from "ramda";
 
-export const progressBarStepsForMeeting = meeting =>
-  meeting.steps.map((currentStep, index, stepsArray) => {
+export const progressBarStepsForMeeting = meeting => {
+  const lastMeetingStep = meeting.steps[meeting.steps.length - 1];
+  return meeting.steps.map((currentStep, index, stepsArray) => {
     const accumulatedPosition = stepsArray
       .slice(0, index)
-      .reduce((acc, curr) => acc + (curr.duration / meeting.totalDuration) * 100, 0);
+      .reduce(
+        (acc, curr) =>
+          acc + (curr.duration / (meeting.totalDuration - lastMeetingStep.duration)) * 100,
+        0,
+      );
+
     return {
       accomplished: currentStep.orderIndex < meeting.currentStep,
       position: index === stepsArray.length - 1 ? 100 : accumulatedPosition,
@@ -13,6 +19,7 @@ export const progressBarStepsForMeeting = meeting =>
       duration: currentStep.duration * 60,
     };
   });
+};
 
 export const stepPositionsForMeeting = progressBarStepsInstance =>
   R.map(step => step.position, progressBarStepsInstance);
