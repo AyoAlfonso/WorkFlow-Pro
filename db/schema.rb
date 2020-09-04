01d52c12135a463b1abbb203b27a3d76aa1f8d15
Sunny <sunny@laterolabs.com>
2020-09-04 12:25:18 -0700

    no hstore in PR

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_09_02_223251) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "hstore"
   enable_extension "plpgsql"
 
   create_table "action_text_rich_texts", force: :cascade do |t|
