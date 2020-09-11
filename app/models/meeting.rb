ab4cc8dbb12505e27475722a509de1f661ee0355
Sunny <sunny@laterolabs.com>
2020-09-11 08:38:52 -0700

    [ch26116] adding scopes for meetings to be more accurate team / user for creation

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -3,13 +3,11 @@ class Meeting < ApplicationRecord
   belongs_to :hosted_by, class_name: "User", optional: true
   belongs_to :meeting_template
 
-  delegate :steps, to: :meeting_template
-  delegate :total_duration, :duration, to: :meeting_template
-  delegate :name, to: :meeting_template
-  delegate :meeting_type, to: :meeting_template
+  delegate :steps, :total_duration, :duration, :name, :meeting_type, to: :meeting_template
 
-  scope :in_progress, -> { where("start_time >= ? AND start_time < ?", Date.today.beginning_of_day.utc, DateTime.now) }
-  scope :for_day, -> (day) { where("Date(created_at) = ?", day) }
+  # scope :in_progress, -> { where("start_time >= ? AND start_time < ?", Date.today.beginning_of_day.utc, DateTime.now) }
+  # scope :for_day, -> (day) { where("Date(created_at) = ?", day) }
+  scope :for_week_of_date, -> (start_time) { where("start_time >= ? AND start_time <= ?", start_time.beginning_of_week, start_time.end_of_week)}
   scope :team_meetings, -> (team_id) { where(team_id: team_id) }
   scope :sort_by_creation_date, -> { order(created_at: :desc)}
   scope :incomplete, -> { where(end_time: nil) }
@@ -18,9 +16,19 @@ class Meeting < ApplicationRecord
   
   scope :hosted_by_user, -> (user) { where(hosted_by_id: user.id)}
   scope :personal_meetings, -> { joins(:meeting_template).where(meeting_templates: {meeting_type: :personal_weekly})}
+  
   #TODO: modify scope to fetch completed meetings if recent, sort by 'type', 'incomplete', and 'date created' to show most recent ones
   scope :personal_recent_or_incomplete_for_user, ->(user) { personal_meetings.hosted_by_user(user).incomplete}
   
   
-  #TODO: ADD FIELD TO LAYER IN MODIFIED MEETING DURATIONS FOR THE AGENDA
+  scope :personal_meeting_for_week_on_user, -> (user, for_week_of_date) { personal_meetings.hosted_by_user(user).for_week_of_date(for_week_of_date) }
+
+  before_create :start_meeting_if_weekly_planning
+
+  private
+  def start_meeting_if_weekly_planning
+    if self.meeting_type == "personal_weekly"
+      self.start_time = Time.now
+    end
+  end
 end
