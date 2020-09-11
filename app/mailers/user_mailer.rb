1af5801395c7eee215a49bbafa0c4fae21423423
Christopher Pang <christopher@laterolabs.com>
2020-09-11 09:42:44 -0700

    redirecting user to my weekly meeting page

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,6 @@
 class UserMailer < ApplicationMailer
   add_template_helper(StatsHelper)
+  include StatsHelper
   default from: 'LynchPyn <no-reply@lynchpyn.com>'
   layout 'mailer'
 
@@ -15,7 +16,7 @@ class UserMailer < ApplicationMailer
     @subject = "Weekly Report"
     @message = params[:message]
     week_to_review_start_time = get_beginning_of_last_or_current_work_week_date(@user.time_in_user_timezone)
-    @meeting = Meeting.first_or_create_for_weekly_planning_on_email(current_user, week_to_review_start_time)
+    @meeting = Meeting.first_or_create_for_weekly_planning_on_email(@user, week_to_review_start_time)
     mail(to: @user.email, subject: @subject)
   end
 end
