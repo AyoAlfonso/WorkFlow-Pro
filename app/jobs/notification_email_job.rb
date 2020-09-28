57cc7546dfb7c518fe3cd382abd3bd7f8fb54f28
Sunny <sunny@laterolabs.com>
2020-09-28 12:42:23 -0700

    [ch22847][ch22851] fixing notification specs and funcitonality for status setting

--- app/jobs/notification_email_job.rb
+++ app/jobs/notification_email_job.rb
@@ -35,7 +35,14 @@ class NotificationEmailJob
 
   def user_has_not_set_status
     most_recent_daily_log = @user.daily_logs.order(:created_at).last
-    if most_recent_daily_log.log_date < Date.today && @users_time >= @user.time_in_user_timezone('noon')
+    #return true if most_recent_daily_log.blank?
+
+    #if the last daily log's log date is less than today, status not set
+    #if the daily log's log date is today in the user's timezone and the status is not set send it.  
+    # (It doesn't do a noon time check in case you want to set the notifcation) - @users_time >= @user.time_in_user_timezone('noon')
+    if most_recent_daily_log && most_recent_daily_log.log_date == @user.time_in_user_timezone.to_date && most_recent_daily_log.work_status != "status_not_set"
+      return false
+    else
       return true
     end
   end
