79a97e2e3bc1305b36431fecd6d246dcb5ded97c
kyleso <kyle@laterolabs.com>
2020-09-29 13:31:56 -0700

    fix migrations

--- db/migrate/20200928183757_create_team_issues.rb
+++ db/migrate/20200928183757_create_team_issues.rb
@@ -4,6 +4,7 @@ class CreateTeamIssues < ActiveRecord::Migration[6.0]
       t.references :issue
       t.references :team
       t.integer :position
+      t.string :completed_at
 
       t.timestamps
     end
@@ -11,7 +12,7 @@ class CreateTeamIssues < ActiveRecord::Migration[6.0]
 
   def data
     Issue.where.not(team_id: nil).sort_by_position_and_priority_and_created_at_and_completed_at.each.with_index(1) do |issue, index|
-      TeamIssue.where(issue_id: issue.id).first_or_create!(issue_id: issue.id, team_id: issue.team_id, position: index)
+      TeamIssue.where(issue_id: issue.id).first_or_create!(issue_id: issue.id, team_id: issue.team_id, position: index, completed_at: issue.completed_at)
     end
   end
 end
