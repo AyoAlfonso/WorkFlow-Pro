4ffc06b006f620e997e8a633db1845203971745a
kyleso <kyle@laterolabs.com>
2020-10-07 14:16:49 -0700

    add data script for migration to change notes fields from nil to empty string

--- db/migrate/20201007010441_add_notes_to_meeting.rb
+++ db/migrate/20201007010441_add_notes_to_meeting.rb
@@ -1,5 +1,14 @@
 class AddNotesToMeeting < ActiveRecord::Migration[6.0]
   def change
-    add_column :meetings, :notes, :text
+    add_column :meetings, :notes, :text, default: ""
+  end
+
+  def data
+    Meeting.all.each do |m|
+      if m.notes.nil?
+        m.notes = ""
+      end
+      m.save!
+    end
   end
 end
