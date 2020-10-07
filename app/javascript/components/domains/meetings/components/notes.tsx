c98e48266eb13a05c4e19631d496ac69a7cc651c
Christopher Pang <christopher@laterolabs.com>
2020-10-06 17:04:32 -0700

    added wysiwyg

--- app/javascript/components/domains/meetings/components/notes.tsx
+++ app/javascript/components/domains/meetings/components/notes.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import styled from "styled-components";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import ContentEditable from "react-contenteditable";
 import { Heading } from "~/components/shared";
 import { useMst } from "~/setup/root";
 import MeetingTypes from "~/constants/meeting-types";
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 interface NotesProps {
   meeting: any;
@@ -16,12 +18,14 @@ export const Notes = ({ meeting }: NotesProps): JSX.Element => {
 
   const meetingType = meeting.meetingType;
 
+  const [editorText, setEditorText] = useState<any>(meeting.notes);
+
   return (
     <Container>
       <Heading type={"h5"} fontSize={"16px"} fontWeight={400} mt={10} mb={10}>
         Enter your notes below
       </Heading>
-      <StyledContentEditable
+      {/* <StyledContentEditable
         innerRef={notesRef}
         html={meeting.notes}
         onChange={e => {
@@ -39,21 +43,39 @@ export const Notes = ({ meeting }: NotesProps): JSX.Element => {
             meetingStore.updateMeeting(meetingObj);
           }
         }}
-      />
+      /> */}
+      <EditorWrapper>
+        <Editor
+          toolbar={{
+            options: ["inline", "list"],
+            inline: {
+              options: ["bold", "italic", "underline", "strikethrough"],
+            },
+            list: {
+              options: ["unordered", "ordered"],
+            },
+          }}
+          //editorState={editorText}
+          onChange={e => setEditorText(e)}
+          onBlur={() => {
+            const meetingObj = { id: meeting.id, notes: editorText };
+            if (meetingType == MeetingTypes.PERSONAL_WEEKLY) {
+              meetingStore.updatePersonalMeeting(meetingObj);
+            } else if (meetingType == MeetingTypes.TEAM_WEEKLY) {
+              meetingStore.updateMeeting(meetingObj);
+            }
+          }}
+        />
+      </EditorWrapper>
     </Container>
   );
 };
 
 const Container = styled.div``;
 
-const StyledContentEditable = styled(ContentEditable)`
-  font-size: 16px;
-  padding-top: 5px;
-  padding-bottom: 5px;
-  padding-left: 4px;
-  padding-right: 4px;
-  margin-right: -4px;
+const EditorWrapper = styled.div`
   height: 300px;
   overflow-y: auto;
   border: ${props => `1px solid ${props.theme.colors.borderGrey}`};
+  padding: 8px;
 `;
