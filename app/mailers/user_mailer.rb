1cbcc03fe2a327247263b1be80e1a80de77cf635
Christopher Pang <christopher@laterolabs.com>
2020-10-14 17:17:07 -0700

    styling for email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,14 +6,14 @@ class UserMailer < ApplicationMailer
 
   def notification_email
     @user = params[:user]
-    @subject = "Notification - #{params[:subject]}"
+    @subject = "#{@user.first_name}, Your Upcoming #{params[:subject}"
     @message = params[:message]
     mail(to: @user.email, subject: @subject)
   end
 
   def end_of_week_stats
     @user = params[:user]
-    @subject = "Weekly Report"
+    @subject = "#{@user.first_name}, Time to Plan for Next Week"
     @message = params[:message]
     week_to_review_start_time = get_beginning_of_last_or_current_work_week_date(@user.time_in_user_timezone)
     @meeting = Meeting.first_or_create_for_weekly_planning_on_email(@user, week_to_review_start_time)
