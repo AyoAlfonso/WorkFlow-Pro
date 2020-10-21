c8630d38f491d0a1e4808c7737585599c78aad4f
Sunny <sunny@laterolabs.com>
2020-10-21 13:14:26 -0700

    fixing base email for system messages

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -1,6 +1,6 @@
 class CustomDeviseMailer < Devise::Mailer
   def invitation_instructions(record, token, opts={})
-    opts[:from] = "LynchPyn"
+    opts[:from] = "LynchPyn <no-reply@lynchpyn.com>"
     opts[:subject] = "Invitation to join #{Company.find(record.company_id).name} on LynchPyn"
     super
   end
