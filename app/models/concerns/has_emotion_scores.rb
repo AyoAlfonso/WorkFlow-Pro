2ec7fcdead66021735d4cf4436ea66d873dcd429
Christopher Pang <christopher@laterolabs.com>
2020-11-09 11:19:37 -0800

    removed todatetime so it wouldn't convert to UTC 0

--- app/models/concerns/has_emotion_scores.rb
+++ app/models/concerns/has_emotion_scores.rb
@@ -6,11 +6,11 @@ module HasEmotionScores
     q_attempts = QuestionnaireAttempt.where(user: users)
                                       .where("emotion_score IS NOT NULL AND completed_at <= ? AND completed_at >= ?", to_date, from_date)
                                       .select(:id, :completed_at, :emotion_score)
-                                      .group_by{|qa| qa.completed_at.to_datetime}
+                                      .group_by{|qa| qa.completed_at}
     results_array = []
     q_attempts.map do |qa|
       average_score_hash = {
-        date: qa[0].to_datetime,
+        date: qa[0],
         average_score: qa[1].pluck(:emotion_score).inject(:+).to_f / qa[1].size
       }
       results_array << average_score_hash
@@ -19,7 +19,7 @@ module HasEmotionScores
     record_dates = results_array.pluck(:date).map do |date|
       date.strftime("%a-%-d")
     end
-
+    
     {
       emotion_scores: results_array,
       record_dates: record_dates
