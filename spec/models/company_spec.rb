c8e53435142338de1d1b1cfd3eb3108c9d52366e
Christopher Pang <christopher@laterolabs.com>
2021-01-04 15:01:41 -0800

    fix for wrong initiative dates

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -30,11 +30,11 @@ RSpec.describe Company, type: :model do
       end
 
       it "has correct next fiscal start date" do
-        expect(company_1.next_fiscal_start_date).to eq Date.new(2021,1,1)
-        expect(company_2.next_fiscal_start_date).to eq Date.new(2020,9,1)+13.weeks
-        expect(company_3.next_fiscal_start_date).to eq Date.new(2020,11,1)
-        expect(company_4.next_fiscal_start_date).to eq Date.new(2020,7,1)+26.weeks
-        expect(company_5.next_fiscal_start_date).to eq Date.new(2020,2,1)+39.weeks
+        expect(company_1.next_quarter_start_date).to eq Date.new(2021,1,1)
+        expect(company_2.next_quarter_start_date).to eq Date.new(2020,9,1)+13.weeks
+        expect(company_3.next_quarter_start_date).to eq Date.new(2020,11,1)
+        expect(company_4.next_quarter_start_date).to eq Date.new(2020,7,1)+26.weeks
+        expect(company_5.next_quarter_start_date).to eq Date.new(2020,2,1)+39.weeks
       end
 
     end
@@ -55,6 +55,49 @@ RSpec.describe Company, type: :model do
         expect(company_2.current_fiscal_quarter).to eq 1
         expect(company_3.current_fiscal_quarter).to eq 4
       end
+    end
+
+    after :each do
+      Timecop.return
+    end
+  end
+
+
+  describe "current fiscal year date for beginning of year" do
+    before :each do
+      Timecop.freeze(2021, 1, 2) 
+    end 
+    context "company has an old fiscal year start" do
+      let(:company_1) {create(:company, fiscal_year_start: "2017-01-01")}
+      let(:company_2) {create(:company, fiscal_year_start: "2017-09-01")}
+      let(:company_3) {create(:company, fiscal_year_start: "2017-11-01")}
+      let(:company_4) {create(:company, fiscal_year_start: "2017-07-01")}
+      let(:company_5) {create(:company, fiscal_year_start: "2017-02-01")}
+
+      it "uses the right year" do
+        expect(company_1.current_fiscal_year).to eq 2021
+        expect(company_2.current_fiscal_year).to eq 2021
+        expect(company_3.current_fiscal_year).to eq 2021
+        expect(company_4.current_fiscal_year).to eq 2021
+        expect(company_5.current_fiscal_year).to eq 2021
+      end
+
+      it "has the right quarter" do
+        expect(company_1.current_fiscal_quarter).to eq 1
+        expect(company_2.current_fiscal_quarter).to eq 2
+        expect(company_3.current_fiscal_quarter).to eq 1
+        expect(company_4.current_fiscal_quarter).to eq 3
+        expect(company_5.current_fiscal_quarter).to eq 4
+      end
+
+      it "has correct next fiscal start date" do
+        expect(company_1.next_quarter_start_date).to eq Date.new(2021,1,1)+13.weeks
+        expect(company_2.next_quarter_start_date).to eq Date.new(2020,9,1)+26.weeks
+        expect(company_3.next_quarter_start_date).to eq Date.new(2020,11,1)+13.weeks
+        expect(company_4.next_quarter_start_date).to eq Date.new(2020,7,1)+39.weeks
+        expect(company_5.next_quarter_start_date).to eq Date.new(2021,2,1)
+      end
+
     end
 
     after :each do
