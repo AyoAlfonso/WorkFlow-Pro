130d8940588faf4705f3b81cddca2a67bcea6ebc
Christopher Pang <christopher@laterolabs.com>
2021-01-22 15:57:00 -0800

    updated scopes

--- app/controllers/api/goals_controller.rb
+++ app/controllers/api/goals_controller.rb
@@ -5,7 +5,7 @@ class Api::GoalsController < Api::ApplicationController
     @company = Company.find(current_user.current_selected_company_id)
     @company_current_fiscal_year = @company.current_fiscal_year
     @user_goals = policy_scope(AnnualInitiative).owned_by_user(current_user).for_company_current_year_and_future(@company_current_fiscal_year)
-    @company_goals = policy_scope(AnnualInitiative).for_users_company(current_user).for_company_current_year_and_future(@company_current_fiscal_year)
+    @company_goals = policy_scope(AnnualInitiative).user_current_company(current_user).for_company_current_year_and_future(@company_current_fiscal_year)
     @personal_vision = current_user[:personal_vision]
     render 'api/goals/index'
   end
