cb3c980caeb707898d81e523ae47da0b010773ed
Mani Jafari <mani@laterolabs.com>
2021-01-28 12:13:36 -0800

    [ch29454] Complete the exploration topic component with showing the exploration topic related to the current meeting in progress

--- app/javascript/components/domains/meetings-forum/components/exploration-topic.tsx
+++ app/javascript/components/domains/meetings-forum/components/exploration-topic.tsx
@@ -1,22 +1,49 @@
 import * as React from "react";
+import * as R from "ramda";
 import styled from "styled-components";
 import { HeaderText } from "~/components/shared/styles/container-header";
+import { Text } from "~/components/shared/text";
+import { Avatar } from "~/components/shared/avatar";
 import { useTranslation } from "react-i18next";
+import { ColumnContainer, Container } from "./row-style";
+import { useMst } from "~/setup/root";
+import { observer } from "mobx-react";
 
-export const ExplorationTopic = (): JSX.Element => {
+export const ExplorationTopic = observer((): JSX.Element => {
   const { t } = useTranslation();
+  const { meetingStore } = useMst();
+  const meeting = meetingStore.currentMeeting;
+
+  const explorationTopic = R.path(["forumExplorationTopic"], meeting.settings);
 
   return (
-    <Container>
+    <MainContainer>
       <HeaderContainer>
         <HeaderTextContent>{t("meetingForum.explorationTopic.whoTitle")}</HeaderTextContent>
         <HeaderTextContent>{t("meetingForum.explorationTopic.topicTitle")}</HeaderTextContent>
       </HeaderContainer>
-    </Container>
+      <Container>
+            <ColumnContainer>
+              <Avatar
+                firstName={"sample"}
+                lastName={"sample"}
+                size={48}
+                marginLeft={"inherit"}
+                marginRight={"inherit"}
+              />
+              <Text>Sample sample</Text>
+            </ColumnContainer>
+            <ColumnContainer>
+              <ExplorationTopicContent>
+                {explorationTopic}
+              </ExplorationTopicContent>
+            </ColumnContainer>
+          </Container>
+    </MainContainer>
   );
-};
+});
 
-const Container = styled.div`
+const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
 `;
@@ -32,5 +59,18 @@ const HeaderContainer = styled.div`
 
 const HeaderTextContent = styled(HeaderText)`
   text-align: left;
+  justify-content: start;
   margin-right: auto;
+  margin-left: 16px;
+  min-width: 240px;
+`;
+
+const ExplorationTopicContent = styled(Text)`
+  padding-top: 5px;
+  padding-bottom: 5px;
+  border-radius: 10px;
+  border: 1px solid #e3e3e3;
+  box-shadow: 0px 3px 6px #f5f5f5;
+  padding-left: 16px;
+  padding-right: 16px;
 `;
