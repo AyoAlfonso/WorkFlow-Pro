20dad08b7aff7df63b7451df41211e5eb250b737
Mani Jafari <mani@laterolabs.com>
2021-01-28 14:59:56 -0800

    [ch29398] Finalize the exploration topic component to show empty user avatar and empty topic placeholder when a topic or user haven't been assigned.

--- app/javascript/components/domains/meetings-forum/components/exploration-topic.tsx
+++ app/javascript/components/domains/meetings-forum/components/exploration-topic.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import * as R from "ramda";
+import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { HeaderText } from "~/components/shared/styles/container-header";
-import { Text } from "~/components/shared/text";
+import { Text, Icon, Loading } from "~/components/shared";
 import { Avatar } from "~/components/shared/avatar";
 import { useTranslation } from "react-i18next";
 import { ColumnContainer, Container } from "./row-style";
@@ -11,10 +12,23 @@ import { observer } from "mobx-react";
 
 export const ExplorationTopic = observer((): JSX.Element => {
   const { t } = useTranslation();
-  const { meetingStore } = useMst();
+  const { 
+    meetingStore, 
+    teamStore: { currentTeam },
+  } = useMst();
   const meeting = meetingStore.currentMeeting;
 
+  if (R.isNil(currentTeam)) {
+    return <Loading />;
+  }
+
   const explorationTopic = R.path(["forumExplorationTopic"], meeting.settings);
+  const currentTeamUsers = currentTeam.users;
+
+  const explorationTopicOwner = currentTeamUsers.find(
+    member => member.id == R.path(["forumExplorationTopicOwnerId"], meeting.settings),
+  );
+
 
   return (
     <MainContainer>
@@ -24,24 +38,62 @@ export const ExplorationTopic = observer((): JSX.Element => {
       </HeaderContainer>
       <Container>
             <ColumnContainer>
-              <Avatar
-                firstName={"sample"}
-                lastName={"sample"}
-                size={48}
-                marginLeft={"inherit"}
-                marginRight={"inherit"}
-              />
-              <Text>Sample sample</Text>
+              { R.isNil(explorationTopicOwner) ? (
+                <>
+                  <ImageContainer>
+                    <Icon icon={"User"} size={"30px"} iconColor={"grey80"} />
+                  </ImageContainer>
+                  <AddMemberText>{t("meetingForum.explorationTopic.noTopicOwner")}</AddMemberText>
+                </>
+
+              ) : (
+                <>
+                  <Avatar
+                    firstName={explorationTopicOwner.firstName}
+                    lastName={explorationTopicOwner.lastName}
+                    defaultAvatarColor={explorationTopicOwner.defaultAvatarColor}
+                    avatarUrl={explorationTopicOwner.avatarUrl}
+                    size={48}
+                    marginLeft={"inherit"}
+                    marginRight={"inherit"}
+                  />
+                  <HostedByName>{`${explorationTopicOwner.firstName} ${explorationTopicOwner.lastName}`}</HostedByName>
+                </>
+                )
+              }
             </ColumnContainer>
             <ColumnContainer>
-              <ExplorationTopicContent>
-                {explorationTopic}
+              <ExplorationTopicContent explorationTopic={explorationTopic} >
+                { R.isNil(explorationTopic) ? (
+                    t("meetingForum.explorationTopic.noTopic")
+                  ) : (
+                    {explorationTopic}
+                  )
+                }
               </ExplorationTopicContent>
             </ColumnContainer>
           </Container>
     </MainContainer>
   );
 });
+
+const ImageContainer = styled.div`
+  border-radius: 9999px;
+  border: ${props => `3px solid ${props.theme.colors.grey80}`};
+  width: 42px;
+  height: 42px;
+  display: flex;
+  justify-content: center;
+`;
+
+const HostedByName = styled(Text)`
+  margin-left: 15px;
+`;
+
+const AddMemberText = styled(HostedByName)`
+  font-style: italic;
+  color: ${props => props.theme.colors.grey80};
+`;
 
 const MainContainer = styled.div`
   display: flex;
@@ -65,7 +117,13 @@ const HeaderTextContent = styled(HeaderText)`
   min-width: 240px;
 `;
 
-const ExplorationTopicContent = styled(Text)`
+type ExplorationTopic = {
+  explorationTopic?: string
+}
+
+const ExplorationTopicContent = styled.p<ExplorationTopic>`
+  margin-top: 0px;
+  margin-bottom: 0px;
   padding-top: 5px;
   padding-bottom: 5px;
   border-radius: 10px;
@@ -73,4 +131,8 @@ const ExplorationTopicContent = styled(Text)`
   box-shadow: 0px 3px 6px #f5f5f5;
   padding-left: 16px;
   padding-right: 16px;
+  width: 100%;
+  height: 25px;
+  color: ${props => 
+    !props.explorationTopic ? props.theme.colors.grey60 : props.theme.colors.black };
 `;
