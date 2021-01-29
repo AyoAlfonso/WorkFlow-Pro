cf0868392bb7dae59d73133db35bc54f67a4f7ac
Christopher Pang <christopher@laterolabs.com>
2021-01-29 11:48:04 -0800

    wip - displaying all possible meetings in date range and allowing user to select

--- app/javascript/components/domains/meetings-forum/forum-agenda.tsx
+++ app/javascript/components/domains/meetings-forum/forum-agenda.tsx
@@ -1,6 +1,10 @@
 import * as React from "react";
 import styled from "styled-components";
-import { EntryContainer, ItemListContainer } from "~/components/shared/journals-and-notes";
+import {
+  EntryContainer,
+  ItemListContainer,
+  ItemCard,
+} from "~/components/shared/journals-and-notes";
 import { useTranslation } from "react-i18next";
 import { useState, useEffect } from "react";
 import { addDays } from "date-fns";
@@ -9,8 +13,11 @@ import { useMst } from "~/setup/root";
 import * as R from "ramda";
 import { toJS } from "mobx";
 import { observer } from "mobx-react";
+import * as moment from "moment";
+import { meetingTypeParser } from "~/components/shared/agenda/meeting-type-parser";
+import { Text } from "~/components/shared/text";
 
-export const ForumAgenda = observer(props => {
+export const ForumAgenda = observer(() => {
   const { t } = useTranslation();
   const {
     forumStore,
@@ -20,10 +27,11 @@ export const ForumAgenda = observer(props => {
   const [selectedDateFilter, setSelectedDateFilter] = useState<string>(
     t("dateFilters.lastThirtyDays"),
   );
+  const [selectedMeetingId, setSelectedMeetingId] = useState<number>(null);
 
   const teamId = forumStore.currentForumTeamId || R.path([0, "id"], toJS(teams));
 
-  const defaultStartDate = addDays(new Date(), -15);
+  const defaultStartDate = addDays(new Date(), -30);
   const defaultEndDate = new Date();
 
   const [dateFilter, setDateFilter] = useState<any>({
@@ -61,7 +69,22 @@ export const ForumAgenda = observer(props => {
   };
 
   const renderItems = () => {
-    return <ItemsContainer> RENDER ITEMS SECTION </ItemsContainer>;
+    if (!R.isNil(forumStore.searchedForumMeetings)) {
+      return forumStore.searchedForumMeetings.map((meeting, index) => (
+        <MeetingResultContainer>
+          <MeetingDateText>
+            {moment(meeting.scheduledStartTime).format("ddd, MMM D")}
+          </MeetingDateText>
+          <ItemCard
+            key={index}
+            titleText={moment(meeting.scheduledStartTime).format("LT")}
+            bodyText={meetingTypeParser(meeting.meetingType)}
+            onClick={() => setSelectedMeetingId(meeting.id)}
+            selected={!R.isNil(selectedMeetingId) ? selectedMeetingId === meeting.id : false}
+          />
+        </MeetingResultContainer>
+      ));
+    }
   };
 
   return (
@@ -75,15 +98,30 @@ export const ForumAgenda = observer(props => {
         dateSelectAction={dateSelectedAction}
         additionalBodyComponents={<EntryContainer>{renderSelectedEntry()}</EntryContainer>}
       />
-      <ItemListContainer>{renderItems()}</ItemListContainer>
+      <StyledItemListContainer>{renderItems()}</StyledItemListContainer>
     </Container>
   );
 });
 
 const Container = styled.div``;
 
-const ItemsContainer = styled.div``;
-
 const SelectedEntryContainer = styled.div`
   margin-left: 50px;
+`;
+
+const StyledItemListContainer = styled(ItemListContainer)`
+  padding-left: 5px;
+  padding-top: 5px;
+  margin-left: -5px;
+  width: 370px;
+`;
+
+const MeetingResultContainer = styled.div``;
+
+const MeetingDateText = styled(Text)`
+  margin-top: 0;
+  margin-bottom: 10px;
+  margin-left: 8px;
+  font-size: 12px;
+  font-weight: bold;
 `;
---
+++ app/javascript/components/shared/agenda/meeting-type-parser.ts
@@ -0,0 +1,10 @@
+import * as humanizeString from "humanize-string"
+
+export const meetingTypeParser = (meetingType: string) : string => {
+  switch(meetingType) {
+    case "forum_monthly":
+      return "Forum Meeting"
+    default:
+      return humanizeString(meetingType);
+  }
+}
