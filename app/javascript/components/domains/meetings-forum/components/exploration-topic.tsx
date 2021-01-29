b67eb76bbe8c13efac2c188c42f3a5f0a4153469
Sunny <sunny@laterolabs.com>
2021-01-29 08:36:58 -0800

    [ch29398] minor fixes, on finding the current team from a meeting (as currentTeam is not explicitly set in this case), who/topic title from transaltion store in section 1 as well

--- app/javascript/components/domains/meetings-forum/components/exploration-topic.tsx
+++ app/javascript/components/domains/meetings-forum/components/exploration-topic.tsx
@@ -9,29 +9,33 @@ import { useMst } from "~/setup/root";
 import { observer } from "mobx-react";
 import { ForumTopic } from "./forum-topic";
 
-export const ExplorationTopic = observer((): JSX.Element => {
-  const { t } = useTranslation();
-  const { 
-    meetingStore: { currentMeeting }, 
-    teamStore: { currentTeam },
-  } = useMst();
+export const ExplorationTopic = observer(
+  (): JSX.Element => {
+    const { t } = useTranslation();
+    const {
+      meetingStore: { currentMeeting },
+      teamStore: { teams },
+    } = useMst();
+    //a bit roundabout, we sould probably refactor the meeting step to pass in both team and meeting
+    const currentTeam = (teams || []).find(team => team.id === currentMeeting.teamId);
 
-  if (R.isNil(currentTeam) || R.isNil(currentMeeting)) {
-    return <Loading />;
-  }
+    if (R.isNil(currentTeam) || R.isNil(currentMeeting)) {
+      return <Loading />;
+    }
 
-  return (
-    <Container>
-      <HeaderContainer>
-        <HeaderTextContent>{t("meetingForum.explorationTopic.whoTitle")}</HeaderTextContent>
-        <HeaderTextContent>{t("meetingForum.explorationTopic.topicTitle")}</HeaderTextContent>
-      </HeaderContainer>
-      <SectionContainer>
-        <ForumTopic disabled={true} meeting={currentMeeting} teamMembers={currentTeam.users} />
-      </SectionContainer>
-    </Container>
-  );
-});
+    return (
+      <Container>
+        <HeaderContainer>
+          <HeaderTextContent>{t("forum.explorationTopic.whoTitle")}</HeaderTextContent>
+          <HeaderTextContent>{t("forum.explorationTopic.topicTitle")}</HeaderTextContent>
+        </HeaderContainer>
+        <SectionContainer>
+          <ForumTopic disabled={true} meeting={currentMeeting} teamMembers={currentTeam.users} />
+        </SectionContainer>
+      </Container>
+    );
+  },
+);
 
 const Container = styled.div`
   display: flex;
