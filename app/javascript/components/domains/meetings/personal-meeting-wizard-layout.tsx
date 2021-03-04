7d446a344f982f0d94524a3e25f8652574af4099
Sunny <sunny@laterolabs.com>
2021-03-03 22:15:14 -0800

    [ch29529] adding the wizard layout for personal planning.  Added progress bar steps at the bottom as that was what was in the weekly planning (for a matching style).

--- app/javascript/components/domains/meetings/personal-meeting-wizard-layout.tsx
+++ app/javascript/components/domains/meetings/personal-meeting-wizard-layout.tsx
@@ -3,61 +3,39 @@ import styled from "styled-components";
 import { observer } from "mobx-react";
 import { useMst } from "../../../setup/root";
 import { MeetingStep } from "./meeting-step";
-import { MeetingSideOptions } from "./meeting-side-options";
 import { useTranslation } from "react-i18next";
 import { WizardLayout } from "~/components/layouts/wizard-layout";
 import * as R from "ramda";
-import { CoreFourOnly } from "../goals/goals-core-four";
 import { Button } from "~/components/shared/button";
 import { useHistory } from "react-router-dom";
 
 interface ITeamMeetingProps {
   meeting: any;
-  meetingStarted: boolean;
-  startMeetingButton: JSX.Element;
   stopMeetingButton: JSX.Element;
   onNextButtonClick: any;
-  stepsComponent: JSX.Element;
+  stepsComponent?: JSX.Element;
   numberOfSteps: number;
 }
 
-export const MeetingWizardLayout = observer(
+export const PersonalMeetingWizardLayout = observer(
   ({
     meeting,
-    meetingStarted,
-    startMeetingButton,
     stopMeetingButton,
     onNextButtonClick,
-    stepsComponent,
     numberOfSteps,
+    stepsComponent,
   }: ITeamMeetingProps): JSX.Element => {
     const { t } = useTranslation();
-    const { meetingStore } = useMst();
+    // const { meetingStore } = useMst();
     const history = useHistory();
 
-    const meetingTitle = () => {
-      return meetingStarted
-        ? R.path(["currentStepDetails", "name"], meeting)
-        : t("meeting.coreFourTitle");
-    };
+    const meetingTitle = () => R.path(["currentStepDetails", "name"], meeting);
 
-    const meetingDescription = () => {
-      return meetingStarted
-        ? R.path(["currentStepDetails", "instructions"], meeting)
-        : t("meeting.reviewCoreFour");
-    };
+    const meetingDescription = () => R.path(["currentStepDetails", "instructions"], meeting);
 
-    const meetingComponent = () => {
-      return meetingStarted ? (
-        <MeetingStep meeting={meeting} />
-      ) : (
-        <CoreFourWrapper>
-          <CoreFourOnly />
-        </CoreFourWrapper>
-      );
-    };
+    const meetingComponent = () => <MeetingStep meeting={meeting} />;
 
-    const renderMeetingStartedButtons = () => {
+    const actionButtons = () => {
       return (
         <>
           {meeting.currentStep + 1 < numberOfSteps && (
@@ -69,18 +47,14 @@ export const MeetingWizardLayout = observer(
               Next
             </NextButton>
           )}
-          {stopMeetingButton}
+          {meeting.currentStep + 1 >= numberOfSteps && stopMeetingButton}
         </>
       );
     };
 
-    const actionButtons = () => {
-      return meetingStarted ? renderMeetingStartedButtons() : startMeetingButton;
-    };
-
     const closeButtonClick = () => {
-      if (confirm(`Are you sure you want to exit this meeting?`)) {
-        history.push(`/team/${meeting.teamId}`);
+      if (confirm(`Are you sure you want to exit planning?`)) {
+        history.push(`/`);
       }
     };
 
@@ -90,14 +64,11 @@ export const MeetingWizardLayout = observer(
           title={meetingTitle()}
           description={meetingDescription()}
           customActionButton={actionButtons()}
-          childrenUnderDescription={
-            <MeetingSideOptions teamId={meeting.teamId} meeting={meeting} />
-          }
           showSkipButton={false}
           singleComponent={meetingComponent()}
-          customStepsComponent={stepsComponent}
-          showLynchpynLogo={true}
+          showLynchpynLogo={false}
           showCloseButton={true}
+          customStepsComponent={stepsComponent}
           onCloseButtonClick={closeButtonClick}
         />
       </Container>
@@ -107,12 +78,6 @@ export const MeetingWizardLayout = observer(
 
 const Container = styled.div`
   height: 100%;
-`;
-
-const CoreFourWrapper = styled.div`
-  width: 100%;
-  margin-left: 20px;
-  margin-top: 30px;
 `;
 
 const NextButton = styled(Button)`
