a39f91e2075dcff3e3ae0279be508422bec10b2c
Sunny <sunny@laterolabs.com>
2021-03-18 08:22:03 -0700

    [ch30631] adding dotenv to node and adding the ability to enable special menu items for qa testing only

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,9 +2,11 @@ const { environment } = require("@rails/webpacker");
 const jquery = require("./plugins/jquery");
 const typescript = require("./loaders/typescript");
 const customConfig = require("./custom");
+const Dotenv = require("dotenv-webpack");
 
 environment.loaders.prepend("typescript", typescript);
 environment.plugins.prepend("jquery", jquery);
+environment.plugins.append("DotenvPlugin", new Dotenv());
 
 // Merge custom config
 environment.config.merge(customConfig);
