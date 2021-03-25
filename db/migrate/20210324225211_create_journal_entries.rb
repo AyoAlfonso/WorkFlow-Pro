3ff78d47d1dbaaf4bab85bbc8522df44f439a6e3
Sunny <sunny@laterolabs.com>
2021-03-25 13:08:08 -0700

    [ch29469] fix migrations so completed at dates are the created at dates for notes

--- db/migrate/20210324225211_create_journal_entries.rb
+++ db/migrate/20210324225211_create_journal_entries.rb
@@ -16,7 +16,7 @@ class CreateJournalEntries < ActiveRecord::Migration[6.0]
     #utilize the journal serilaizer to convert a json questionnaire attmpet to rich text
     QuestionnaireAttempt.find_each do |qa|
       parsed_body = questionnaire_attempt_to_text(qa.rendered_steps)
-      JournalEntry.create!(generated_from_type: qa.class.name, generated_from_id: qa.id, body: parsed_body, user_id: qa.user_id) if parsed_body.present?
+      JournalEntry.create!(generated_from_type: qa.class.name, generated_from_id: qa.id, body: parsed_body, user_id: qa.user_id, created_at: qa.completed_at) if parsed_body.present?
     end
   end
 end
