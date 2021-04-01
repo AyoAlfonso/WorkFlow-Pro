44aa6aabd460fff742781d7c83677bced54ea27c
Christopher Pang <christopher@laterolabs.com>
2021-04-01 11:45:24 -0700

    commented in code again

--- app/javascript/components/domains/nav/header-bar.tsx
+++ app/javascript/components/domains/nav/header-bar.tsx
@@ -273,23 +273,22 @@ export const HeaderBar = observer(
           <AccountOption id="lynchpyn-whats-new">
             <AccountOptionText>What's New?</AccountOptionText>
           </AccountOption>
-          {console.log("WTF IS THIS", process.env.QA_SHOW_CREATE_COMPANY_FORUM_MENU)}
-          {/* {process.env.QA_SHOW_CREATE_COMPANY_FORUM_MENU == "true" && ( */}
-          <AccountOption
-            style={{ position: "relative" }}
-            onMouseEnter={() => {
-              setShowCompanyCreationSelector(true);
-            }}
-            onMouseLeave={() => {
-              setShowCompanyCreationSelector(false);
-            }}
-          >
-            <AccountOptionText>
-              {!R.isNil(onboardingCompany) ? t("company.edit") : t("company.create")} (QA Only)
-            </AccountOptionText>
-            {renderCompanyCreationSelector()}
-          </AccountOption>
-          {/* )} */}
+          {process.env.QA_SHOW_CREATE_COMPANY_FORUM_MENU == "true" && (
+            <AccountOption
+              style={{ position: "relative" }}
+              onMouseEnter={() => {
+                setShowCompanyCreationSelector(true);
+              }}
+              onMouseLeave={() => {
+                setShowCompanyCreationSelector(false);
+              }}
+            >
+              <AccountOptionText>
+                {!R.isNil(onboardingCompany) ? t("company.edit") : t("company.create")} (QA Only)
+              </AccountOptionText>
+              {renderCompanyCreationSelector()}
+            </AccountOption>
+          )}
           <AccountOption>
             <AccountOptionText onClick={() => sessionStore.logoutRequest()}>
               {t("profile.logout")}
