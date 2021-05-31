0b22197295af31f3fb489e29539deac7960a8904
Mihran Mashhud <mihran@lynchpyn.com>
2021-05-31 13:40:33 -0400

    Fix the overlapping issue of the rallying-cry and personal-vision.

--- app/javascript/components/domains/goals/shared/personal-vision.tsx
+++ app/javascript/components/domains/goals/shared/personal-vision.tsx
@@ -53,12 +53,12 @@ const VisionTitle = styled.p`
   color: ${props => props.theme.colors.primary100};
   margin-top: auto;
   margin-bottom: auto;
-  margin-left: 32px;
-  margin-right: 32px;
+  margin-left: 16px;
+  margin-right: 16px;
   display: flex;
   align-items: center;
   height: inherit;
-  position: absolute;
+  white-space: nowrap;
 `;
 
 const StyledContentEditable = styled(ContentEditable)`
@@ -70,4 +70,6 @@ const StyledContentEditable = styled(ContentEditable)`
   padding: 5px;
   text-transform: capitalize;
   font-weight: 700;
+  overflow: hidden;
+  white-space: nowrap;
 `;
