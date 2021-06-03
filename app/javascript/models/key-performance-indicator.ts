4c7b7371a8817edaa2e266e71ae17bf666d350e4
Ayo <ayo@lynchpyn.com>
2021-06-03 13:15:15 +0100

    Add initial model structure for KPI and Scorecard

---
+++ app/javascript/models/key-performance-indicator.ts
@@ -0,0 +1,15 @@
+import { types, getRoot } from "mobx-state-tree";
+import { QuarterlyGoalModel } from "./quarterly-goal";
+import { KeyElementModel } from "./key-element";
+import { UserModel } from "./user";
+
+export const KeyPerformanceIndicatorModel = types.model("KeyPerformanceIndicatorModel").props({
+  id: types.identifierNumber,
+  companyId: types.maybeNull(types.number),
+  createdById: types.number,
+  description: types.string,
+  ownedById: types.number,
+  ownedBy: types.maybeNull(UserModel),
+  unitType: types.maybeNull(types.number),
+  isDeleted: types.boolean,
+});
