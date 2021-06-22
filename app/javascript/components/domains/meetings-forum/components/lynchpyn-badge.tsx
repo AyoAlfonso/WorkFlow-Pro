0da694d8a296491067a31f35bc41a3269138efce
Ayo <ayo@lynchpyn.com>
2021-06-22 12:08:37 +0100

    Adds a new stacking context for z-index on forum badge

--- app/javascript/components/domains/meetings-forum/components/lynchpyn-badge.tsx
+++ app/javascript/components/domains/meetings-forum/components/lynchpyn-badge.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { Image, Link } from "rebass";
 
 //to do : Make actual semantic button
-const BadgeContainer = styled.div`
+const ImageContainer = styled.div`
   position: fixed;
   z-index: 99;
   right: 0;
@@ -12,19 +12,25 @@ const BadgeContainer = styled.div`
   top: auto;
   cursor: pointer;
 `;
+const BadgeContainer = styled.div`
+  z-index: 0;
+`;
+
 const lynchPynBadgePath = require("../../../../assets/images/LynchPynBadge.png");
 
 export const LynchPynBadge = (): JSX.Element => {
   return (
-    <Link href="http://go.lynchpyn.com/forum">
-      <BadgeContainer>
-        <Image
-          sx={{
-            height: 36,
-          }}
-          src={lynchPynBadgePath}
-        />
-      </BadgeContainer>
-    </Link>
+    <BadgeContainer>
+      <Link href="http://go.lynchpyn.com/forum">
+        <ImageContainer>
+          <Image
+            sx={{
+              height: 36,
+            }}
+            src={lynchPynBadgePath}
+          />
+        </ImageContainer>
+      </Link>
+    </BadgeContainer>
   );
 };
