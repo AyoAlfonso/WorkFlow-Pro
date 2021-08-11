d86ac101444340e245d973bc754a48c6158b243e
Mihran Mashhud <mihran@lynchpyn.com>
2021-08-11 14:38:39 -0400

    Fix the remaining merge conflict.

--- app/javascript/components/domains/scorecard/scorecards-index.tsx
+++ app/javascript/components/domains/scorecard/scorecards-index.tsx
@@ -3,18 +3,11 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useMst } from "../../../setup/root";
 import { useParams } from "react-router-dom";
-<<<<<<< HEAD
 import { Loading } from "../../shared/loading"
 import { ScorecardTableView } from "./scorecard-table-view"
 import { ScorecardSelector } from "./scorecard-selector"
 import { ScorecardSummary } from "./scorecard-summary"
 import { toJS } from "mobx"
-=======
-import { Loading } from "../../shared/loading";
-import { ScorecardTableView } from "./scorecard-table-view";
-import { ScorecardSelector } from "./scorecard-selector";
-import { toJS } from "mobx";
->>>>>>> 266f8aa32719f14755e58cb520515d66eb6b9942
 
 export const ScorecardsIndex = observer(
   (): JSX.Element => {
