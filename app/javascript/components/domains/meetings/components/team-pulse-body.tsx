15fc6f97a14d127f6529654af8b71374e803738b
Ayo Alfonso <ayo@lynchpyn.com>
2021-08-18 21:05:53 +0100

    Remove console.log

--- app/javascript/components/domains/meetings/components/team-pulse-body.tsx
+++ app/javascript/components/domains/meetings/components/team-pulse-body.tsx
@@ -31,9 +31,7 @@ export const TeamPulseBody = ({ meeting }: ITeamPulseBodyProps): JSX.Element => 
     percentageDifference = meeting.emotionScorePercentageDifferenceMonthly;
     periodDesc = "month";
   }
-  teamEmotions = 0;
-  userEmotions = [];
-  console.log(userEmotions, "userEmotions");
+ 
   return (
     <Container>
       {teamEmotions > 0 && userEmotions.length ? (
