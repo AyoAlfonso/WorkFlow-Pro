828f6b33eb8d0ef74ae34eb51abd59dd516cfb24
Ayo Alfonso <ayo@lynchpyn.com>
2021-09-30 12:51:45 +0100

    Fixes description not displaying properly for KPI View/edit Modal

--- app/javascript/components/domains/meetings/components/recap.tsx
+++ app/javascript/components/domains/meetings/components/recap.tsx
@@ -48,11 +48,11 @@ export const Recap = observer(
     const goalProgress = averageMilestoneProcessPercentage;
 
     // key activities that belong to the current meeting's meeting_template and team
-    const tasksCompletedCount = keyActivities.filter(ka => ka.completedAt !== null).length;
+    const tasksCompletedCount = keyActivities?.filter(ka => ka.completedAt !== null).length;
     const totalTasksCount = keyActivities.length;
 
     // issues that belong to this team
-    const issuesCompletedCount = issues.filter(issue => issue.completedAt !== null).length;
+    const issuesCompletedCount = issues?.filter(issue => issue.completedAt !== null).length;
     const ISSUES_TARGET_PER_WEEK = 3;
 
     const cardProps = {
