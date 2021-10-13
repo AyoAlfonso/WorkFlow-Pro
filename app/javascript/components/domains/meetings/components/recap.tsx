e9983143c31fbafcb1c3c20f6604c7b0ee4543ba
Ayo Alfonso <ayo@lynchpyn.com>
2021-10-13 16:26:08 +0100

    Adds an avatar to the meeting pyns, renames planning nav bar header, summary percentage color code corrected

--- app/javascript/components/domains/meetings/components/recap.tsx
+++ app/javascript/components/domains/meetings/components/recap.tsx
@@ -37,18 +37,14 @@ export const Recap = observer(
       return <Loading />;
     }
 
-    const {
-      keyActivities,
-      issues,
-      averageMilestoneProcessPercentage,
-    } = meetingStore.meetingRecap;
+    const { keyActivities, issues, averageMilestoneProcessPercentage } = meetingStore.meetingRecap;
 
     // the average completed percentage of milestones that belong to the quarterly goals that belong to the users of the team for the current week
     const goalProgress = averageMilestoneProcessPercentage || parseInt("0");
 
     // key activities that belong to the current meeting's meeting_template and team
     const tasksCompletedCount = keyActivities?.filter(ka => ka.completedAt !== null).length || 0;
-    const totalTasksCount = keyActivities?.length || 1 ;
+    const totalTasksCount = keyActivities?.length || 1;
 
     // issues that belong to this team
     const issuesCompletedCount = issues?.filter(issue => issue.completedAt !== null).length || 0;
