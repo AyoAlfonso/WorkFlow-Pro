dcbe3ec461d13d6dba72bc8f019175d6b4204591
Ayo Alfonso <ayo@lynchpyn.com>
2021-11-01 11:33:31 +0100

    Add the enablements missing field and toggle update_all feature

--- app/models/team_user_enablement.rb
+++ app/models/team_user_enablement.rb
@@ -1,6 +1,12 @@
 class TeamUserEnablement < ApplicationRecord
+  before_save :falsify_all_others
+  
   belongs_to :user
   belongs_to :team
 
   enum role: { team_member: 0, team_lead: 1 }
+
+  def falsify_all_others
+    self.class.where('id != ? AND team_id = ?', self.id, self.team_id).update_all("team_manager = 'false'")
+  end
 end
