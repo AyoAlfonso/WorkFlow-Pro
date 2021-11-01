41250d9329d66f39dc72756596fb3bc1604219e7
Ayo Alfonso <ayo@lynchpyn.com>
2021-11-01 12:15:56 +0100

    Moved the fucntion call to the application level

--- app/models/team_user_enablement.rb
+++ app/models/team_user_enablement.rb
@@ -1,12 +1,11 @@
 class TeamUserEnablement < ApplicationRecord
-  before_save :falsify_all_others
-  
+
   belongs_to :user
   belongs_to :team
 
   enum role: { team_member: 0, team_lead: 1 }
 
-  def falsify_all_others
+  def falsify_all_others_and_update_manager
     self.class.where('id != ? AND team_id = ?', self.id, self.team_id).update_all("team_manager = 'false'")
   end
 end
