61b9cf12c9ee2fdac15d6be9c4d24f6c222c3ede
Ayo Alfonso <ayo@lynchpyn.com>
2021-11-18 23:28:42 +0100

    Format getNotification func

--- app/javascript/components/domains/account/notifications/utils.tsx
+++ app/javascript/components/domains/account/notifications/utils.tsx
@@ -1,16 +1,15 @@
-
- export const getNoticationName = type => {
-      let notificationName = type;
-      switch (notificationName) {
-        case "Daily Planning":
-          notificationName = "Today's Focus";
-          break;
-        case "Weekly Planning":
-          notificationName = "Weekly Review";
-          break;
-        case "Weekly Checkin":
-          notificationName = "Weekly Check In";
-          break;
-      }
-      return notificationName;
+export const getNoticationName = type => {
+  let notificationName = type;
+  switch (notificationName) {
+    case "Daily Planning":
+      notificationName = "Today's Focus";
+      break;
+    case "Weekly Planning":
+      notificationName = "Weekly Review";
+      break;
+    case "Weekly Checkin":
+      notificationName = "Weekly Check In";
+      break;
+  }
+  return notificationName;
 };
