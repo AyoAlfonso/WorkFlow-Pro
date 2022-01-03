5116b54c8d2ed29b1af74a8a988ac67dabef2e29
Ayo Alfonso <ayo@lynchpyn.com>
2022-01-03 21:25:46 +0100

    Add the node-sass module

--- babel.config.js
+++ babel.config.js
@@ -29,6 +29,8 @@ module.exports = function(api) {
       ],
       (isProductionEnv || isDevelopmentEnv) && [
         "@babel/preset-env",
+        "@babel/preset-react",
+        "@babel/preset-flow",
         {
           forceAllTransforms: true,
           useBuiltIns: "entry",
