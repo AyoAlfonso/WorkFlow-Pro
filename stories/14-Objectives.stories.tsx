32ab956b669abe9ddb8793146b3447c9007b939c
aushtiin <aotangban@gmail.com>
2022-01-05 12:08:36 +0100

    tests for initiative and objectives

--- stories/14-Objectives.stories.tsx
+++ stories/14-Objectives.stories.tsx
@@ -11,9 +11,14 @@ import { Router } from "react-router-dom";
 import styled from "styled-components";
 import { useMst } from "~/setup/root";
 import * as moment from "moment";
+import * as R from "ramda";
 import { WeeklyMilestones } from "~/components/domains/goals/shared-quarterly-goal-and-sub-initiative/weekly-milestones";
 import { KeyElement } from "~/components/domains/goals/shared/key-element";
-import { Button, Icon, TextDiv } from "~/components/shared";
+import { Avatar, Button, Icon, Text, TextDiv } from "~/components/shared";
+import { ActivityLogs } from "~/components/domains/goals/shared/activity-logs";
+import { StatusBlockColorIndicator } from "~/components/domains/goals/shared/status-block-color-indicator";
+import { HomeContainerBorders } from "~/components/domains/home/shared-components";
+import { RecordOptions } from "~/components/domains/goals/shared/record-options";
 
 export default { title: "Objectives" };
 
@@ -470,12 +475,20 @@ const goalYearString = `FY${quarterlyGoal.fiscalYear.toString().slice(-2)}/${(
   .slice(-2)}`;
 
 export const Objectives = () => {
-
   const [showInactiveMilestones, setShowInactiveMilestones] = React.useState<boolean>(false);
   const [showMilestones, setShowMilestones] = React.useState<boolean>(true);
   const [showKeyElementForm, setShowKeyElementForm] = React.useState<boolean>(false);
   const [showActionType, setActionType] = React.useState<string>("Add");
   const [selectedElement, setSelectedElement] = React.useState<number>(null);
+
+  const renderContextTabs = async () => (
+    <Context
+      activeInitiatives={quarterlyGoal.subInitiatives.length}
+      setShowMilestones={setShowMilestones}
+      itemType={"quarterlyGoal"}
+      item={quarterlyGoal}
+    />
+  );
 
   return (
     <Provider value={rootStore}>
@@ -561,6 +574,106 @@ export const Objectives = () => {
             </StyledButton>
           </Container>
         </ContainerDiv>
+        <ContainerDiv>
+          <h1>Initiative header</h1>
+          <CodeBlockDiv mb={"20px"}>
+            <CopyBlock
+              text={`
+              import * as React from "react";
+              import { InitiativeHeader } from "../app/javascript/components/domains/goals/shared-quarterly-goal-and-sub-initiative/initiative-header";
+            
+              <InitiativeHeader
+                itemType={itemType}
+                item={quarterlyGoal}
+                editable={editable}
+                setAnnualInitiativeId={setAnnualInitiativeId}
+                setModalOpen={setQuarterlyGoalModalOpen}
+                setAnnualInitiativeModalOpen={setAnnualInitiativeModalOpen}
+                annualInitiativeId={quarterlyGoal.annualInitiativeId}
+                annualInitiativeDescription={annualInitiativeDescription}
+                showDropdownOptionsContainer={showDropdownOptionsContainer}
+                setShowDropdownOptionsContainer={setShowDropdownOptionsContainer}
+                goalYearString={goalYearString}
+                derivedStatus={getCurrentWeekStatus()}
+              />
+              )
+            `}
+              language={"tsx"}
+              theme={atomOneLight}
+            />
+          </CodeBlockDiv>
+          <Container>
+            <InitiativeHeader
+              itemType={"quarterlyGoal"}
+              item={quarterlyGoal}
+              editable={true}
+              annualInitiativeId={quarterlyGoal.annualInitiativeId}
+              annualInitiativeDescription={annualInitiativeDescription}
+              goalYearString={goalYearString}
+            />
+          </Container>
+        </ContainerDiv>
+        <ContainerDiv>
+          <h1>Context Tabs</h1>
+          <CodeBlockDiv mb={"20px"}>
+            <CopyBlock
+              text={`
+              import * as React from "react";
+              import { Context } from "../app/javascript/components/domains/goals/shared-quarterly-goal-and-sub-initiative/context";
+            
+              <Context
+                activeInitiatives={quarterlyGoal.subInitiatives.length}
+                setShowInitiatives={setShowInitiatives}
+                setShowMilestones={setShowMilestones}
+                itemType={itemType}
+                item={quarterlyGoal}
+              />
+              )
+            `}
+              language={"tsx"}
+              theme={atomOneLight}
+            />
+          </CodeBlockDiv>
+          <Container>
+            <Context
+              activeInitiatives={quarterlyGoal.subInitiatives.length}
+              setShowMilestones={setShowMilestones}
+              itemType={"quarterlyGoal"}
+              item={quarterlyGoal}
+            />
+          </Container>
+        </ContainerDiv>
+        <ContainerDiv>
+          <h1>Quarterly Goal Card</h1>
+          <QuarterlyGoalContainer>
+            <StatusBlockColorIndicator
+              milestones={quarterlyGoal.milestones || []}
+              indicatorWidth={"80px"}
+              indicatorHeight={4}
+              marginBottom={16}
+            />
+
+            <TopRowContainer>
+              <QuarterlyGoalDescription>{quarterlyGoal.description}</QuarterlyGoalDescription>
+              <QuarterlyGoalOptionContainer>
+                <RecordOptions type={"quarterlyGoal"} id={quarterlyGoal.id} />
+              </QuarterlyGoalOptionContainer>
+            </TopRowContainer>
+            <BottomRowContainer>
+              {quarterlyGoal.ownedBy && (
+                <QuarterlyGoalOwnerContainer>
+                  <Avatar
+                    firstName={R.path(["ownedBy", "firstName"], quarterlyGoal)}
+                    lastName={R.path(["ownedBy", "lastName"], quarterlyGoal)}
+                    defaultAvatarColor={R.path(["ownedBy", "defaultAvatarColor"], quarterlyGoal)}
+                    avatarUrl={R.path(["ownedBy", "avatarUrl"], quarterlyGoal)}
+                    size={40}
+                  />
+                </QuarterlyGoalOwnerContainer>
+              )}
+            </BottomRowContainer>
+          </QuarterlyGoalContainer>
+        </ContainerDiv>
       </Router>
     </Provider>
   );
@@ -600,4 +713,36 @@ const AddKeyElementText = styled(TextDiv)`
   white-space: break-spaces;
   color: ${props => props.theme.colors.primary100};
   font-size: 12px;
+`;
+
+const QuarterlyGoalContainer = styled(HomeContainerBorders)`
+  padding: 16px;
+  padding-top: 0;
+  margin-bottom: 16px;
+`;
+
+const TopRowContainer = styled.div`
+  display: flex;
+`;
+
+const BottomRowContainer = styled.div`
+  display: flex;
+  margin-top: 10px;
+`;
+
+const QuarterlyGoalOwnerContainer = styled.div`
+  margin-left: auto;
+`;
+
+const QuarterlyGoalDescription = styled(Text)`
+  margin-top: 0;
+  &:hover {
+    cursor: pointer;
+    font-weight: bold;
+    text-decoration: underline;
+  }
+`;
+
+const QuarterlyGoalOptionContainer = styled.div`
+  margin-left: auto;
 `;
