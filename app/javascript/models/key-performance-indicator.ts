20a73f29313b2f4e0bdab91399e4b5ce7133463d
Ayo Alfonso <ayo@lynchpyn.com>
2022-01-11 20:44:05 +0100

    Handle Error on check-in module

--- app/javascript/models/key-performance-indicator.ts
+++ app/javascript/models/key-performance-indicator.ts
@@ -22,7 +22,7 @@ export const KeyPerformanceIndicatorModel = types
     parentKpi: types.maybeNull(types.array(types.frozen<object>())),
     parentType: types.maybeNull(types.string),
     relatedParentKpis: types.maybeNull(types.array(types.frozen<object>())),
-    viewers: types.maybeNull(types.array(types.model(types.frozen<object>()))),
+    viewers: types.maybeNull(types.array(types.frozen<object>())),
     period: types.maybeNull(types.map(types.map(ScorecardLogModel))),
     scorecardLogs: types.maybeNull(types.array(ScorecardLogModel)),
     weeks: types.maybeNull(types.map(ScorecardLogModel)),
