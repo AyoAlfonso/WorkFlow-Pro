718b11dc8b6723dc300778a43000400662f12cbb
Ayo Alfonso <ayo@lynchpyn.com>
2022-01-19 10:14:36 +0100

    Add a simple update method to replate create_or_update on the Milestone model ast Objective log create

--- app/controllers/api/objective_logs_controller.rb
+++ app/controllers/api/objective_logs_controller.rb
@@ -32,8 +32,9 @@ class Api::ObjectiveLogsController < Api::ApplicationController
   unless existing_milestone.present?
     if (params[:objecteable_type] == "QuarterlyGoal")
      QuarterlyGoal.find(params[:objecteable_id]).create_milestones_for_quarterly_goal(current_user, current_company)
-    #We should eagerly update the current week i.e params[:week] that is being logged
-     Milestone.create_or_update(
+  
+     #We should eagerly update the current week i.e params[:week] that is being logged
+     Milestone.update!(
         milestoneable_type: params[:objecteable_type],
         milestoneable_id: params[:objecteable_id] ,
         week: week,
@@ -44,7 +45,7 @@ class Api::ObjectiveLogsController < Api::ApplicationController
       )
     elsif (params[:objecteable_type] == "SubInitiative")
         SubInitiative.find(params[:objecteable_id]).create_milestones_for_sub_initiative(current_user, current_company)
-        Milestone.create_or_update(
+        Milestone.update!(
         milestoneable_type: params[:objecteable_type],
         milestoneable_id: params[:objecteable_id] ,
         week: week,
