60b80270e7d8f0de928e9c5d6986035207b26590
Ayo Alfonso <ayo@lynchpyn.com>
2022-02-08 12:48:49 +0100

    Prune dependencies

--- app/javascript/components/domains/account/avatarModal.tsx
+++ app/javascript/components/domains/account/avatarModal.tsx
@@ -4,7 +4,6 @@ import { useTranslation } from "react-i18next";
 import { baseTheme } from "../../../themes";
 import styled from "styled-components";
 import { getCroppedImg } from "~/lib/cropImage";
-import "~/stylesheets/utilities.css";
 import Cropper from "react-easy-crop";
 import { Button } from "~/components/shared/button";
 
