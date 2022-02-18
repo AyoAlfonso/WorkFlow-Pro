d06a688abe665529cc50ebbc35d61751e5b6995b
aushtiin <aotangban@gmail.com>
2022-02-18 21:59:15 +0100

    edited schema

--- db/migrate/20210511164820_create_key_performance_indicators.rb
+++ db/migrate/20210511164820_create_key_performance_indicators.rb
@@ -4,9 +4,9 @@ class CreateKeyPerformanceIndicators < ActiveRecord::Migration[6.0]
       t.string :description
       t.datetime :closed_at
       t.references :created_by, references: :user
-      t.references :user, references: :user, foreign_key: true
-      t.references :company, foreign_key: true
-      t.references :team, foreign_key: true
+      #t.references :user, references: :user, foreign_key: true
+      #t.references :company, foreign_key: true
+      #t.references :team, foreign_key: true
       
       t.timestamps
     end
