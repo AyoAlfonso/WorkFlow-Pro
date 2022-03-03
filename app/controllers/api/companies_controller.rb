1f304b004b1c407e58be612e1f1dd1addca670ad
Ayo Alfonso <ayo@lynchpyn.com>
2022-03-03 14:51:16 +0100

    Add the missing props to company update endpoint

--- app/controllers/api/companies_controller.rb
+++ app/controllers/api/companies_controller.rb
@@ -57,8 +57,8 @@ class Api::CompaniesController < Api::ApplicationController
     # elsif params[:company][:logo].blank?
     #   @company.logo.purge
     end
-    render json: @company.as_json(only: ["id", "name", "phone_number", "rallying_cry", "fiscal_year_start", "timezone", "preferences", "display_format", "forum_type", "objectives_key_type"],
-                                  methods: ["accountability_chart_content", "strategic_plan_content", "logo_url", "current_fiscal_quarter", "quarter_for_creating_quarterly_goals", "current_fiscal_year", "year_for_creating_annual_initiatives", "fiscal_year_range", "current_quarter_start_date", "next_quarter_start_date", "forum_meetings_year_range", "forum_intro_video"],
+  render json: @company.as_json(only: ["id", "name", "phone_number", "rallying_cry", "fiscal_year_start", "timezone", "preferences", "display_format", "forum_type", "objectives_key_type"],
+                                  methods: ["accountability_chart_content", "strategic_plan_content", "logo_url", "current_fiscal_week", "current_fiscal_quarter", "quarter_for_creating_quarterly_goals", "current_fiscal_year", "year_for_creating_annual_initiatives", "fiscal_year_range", "current_quarter_start_date", "next_quarter_start_date", "forum_meetings_year_range", "forum_intro_video", "forum_types", "objectives_key_types"],
                                   include: {
                                     core_four: { methods: ["core_1_content", "core_2_content", "core_3_content", "core_4_content"] },
                                     sign_up_purpose: { only: ["purpose"] },
