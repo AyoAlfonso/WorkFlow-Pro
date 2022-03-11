e567d69663c299128629296ff75d8606ddfaaa6e
Ayo Alfonso <ayo@lynchpyn.com>
2022-03-11 21:23:27 +0100

    Add the forum  meeting configs

---
+++ app/javascript/components/domains/meetings/shared/team-forum-management-container.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { AccordionSummary } from "~/components/shared/accordion-components";
+import { IconContainerWithPadding } from "~/components/shared/icon";
+import { Icon } from "~/components/shared/icon";
+import styled from "styled-components";
+import { HeaderContainerNoBorder } from "~/components/shared/styles/container-header";
+interface ITeamForumManagementContainerProps {
+  handleMeetingClick: any;
+  titleText?: string;
+  buttonText?: string;
+}
+
+export const TeamForumManagementContainer = ({
+  handleMeetingClick,
+  titleText,
+  buttonText = "Forum Meeting",
+}: ITeamForumManagementContainerProps): JSX.Element => {
+  return (
+    <AccordionSummary
+      onClick={e => {
+        handleMeetingClick();
+      }}
+    >
+      <HeaderContainerNoBorder>
+        <div style={{ width: "30px" }} />
+        <AccordionHeaderText>{buttonText}</AccordionHeaderText>
+      </HeaderContainerNoBorder>
+      <IconContainerWithPadding>
+        <Icon icon={"Team"} size={16} iconColor={"blue"} />
+      </IconContainerWithPadding>
+    </AccordionSummary>
+  );
+};
+
+export const AccordionHeaderText = styled.h4`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  line-height: 20px;
+  font-size: 16px;
+  font-weight: 600;
+  color: ${props => props.theme.colors.blueRibbon};
+`;
