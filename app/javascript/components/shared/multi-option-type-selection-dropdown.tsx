cb4435a6a2caecf5747bf56fb367a000d0afef7f
Daniel Xiao <danielxiao@Daniels-MacBook-Pro.local>
2022-03-17 10:09:10 -0400

    removed comments

--- app/javascript/components/shared/multi-option-type-selection-dropdown.tsx
+++ app/javascript/components/shared/multi-option-type-selection-dropdown.tsx
@@ -25,11 +25,9 @@ function alphabetically(ascending) {
 
   return function (a, b) {
 
-    // equal items sort equally
     if (a === b) {
         return 0;
     }
-    // nulls sort after anything else
     else if (a === null) {
         return 1;
     }
@@ -48,11 +46,9 @@ function alphabetically(ascending) {
     else if (b.type === "company") {
         return 1;
     }
-    // otherwise, if we're ascending, lowest sorts first
     else if (ascending) {
         return a.name < b.name ? -1 : 1;
     }
-    // if descending, highest sorts first
     else { 
         return a.name < b.name ? 1 : -1;
     }
