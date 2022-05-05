60512237bca6b1b7273787926e036b1782ba69b0
Ayo <alfonsoayo7@gmail.com>
2022-05-05 13:00:17 +0100

    Add the sorting for teams

--- app/services/team_issue_resort_service.rb
+++ app/services/team_issue_resort_service.rb
@@ -11,10 +11,12 @@ class TeamIssueResortService < ApplicationService
     #reposition items that are part of meeting as the first ones, then sort the rest by priority
     sorted_meeting_team_issues = @team_issues.where(id: meeting.team_issue_meeting_enablements.pluck(:id)).sort_by_position.sort_by_completed_date
     sorted_team_issues = @team_issues.where.not(id: meeting.team_issue_meeting_enablements.pluck(:id)).sort_by_issue_priority.sort_by_completed_date
+  #  binding.pry
+ 
+    reset_positions(sorted_meeting_team_issues, sorted_team_issues)
     if @sort_type == 'by_upvotes'
       sorted_team_issues = sorted_team_issues.sort_by_issue_upvotes
     end
-    reset_positions(sorted_meeting_team_issues, sorted_team_issues)
   end
 
   def reset_positions(sorted_meeting_team_issues, sorted_team_issues)
