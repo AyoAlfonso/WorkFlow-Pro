f7d435e8e0e18f10cdcb5349c9ab9d7c2e6cd33d
Ayo <alfonsoayo7@gmail.com>
2022-07-13 20:59:10 +0100

    Add the correct date

--- app/controllers/api/check_in_templates_controller.rb
+++ app/controllers/api/check_in_templates_controller.rb
@@ -139,7 +139,7 @@ class Api::CheckInTemplatesController < Api::ApplicationController
   date_time_config = @check_in_template.date_time_config
   check_in_artifacts = [];
 
-  schedule = IceCube::Schedule.new(Time.now)
+  schedule = IceCube::Schedule.new(Time.now - 7.days)
   day_as_int = IceCube::RuleHelper.day_of_week_as_int(date_time_config)
   hour_as_int = IceCube::RuleHelper.hour_of_day_as_int(date_time_config)
   minute_as_int = IceCube::RuleHelper.minute_of_hour_as_int(date_time_config)
@@ -266,7 +266,7 @@ class Api::CheckInTemplatesController < Api::ApplicationController
   end
 
   def artifact
-    # binding.pry
+    binding.pry
   check_in_artifact = CheckInArtifact.find(params[:id])
   @check_in_template = CheckInTemplate.find(check_in_artifact.check_in_template_id)
   date_time_config = @check_in_template.date_time_config
@@ -275,7 +275,7 @@ class Api::CheckInTemplatesController < Api::ApplicationController
   hour_as_int = IceCube::RuleHelper.hour_of_day_as_int(date_time_config)
   minute_as_int = IceCube::RuleHelper.minute_of_hour_as_int(date_time_config)
 
-  schedule = IceCube::Schedule.new(check_in_artifact.start_time)
+  schedule = IceCube::Schedule.new(check_in_artifact.start_time - 7.days)
 
   day = Time.parse(date_time_config["date"]).day if date_time_config["date"].present?
   begin
