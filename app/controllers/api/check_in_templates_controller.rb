8891d9a999e38c035ca3fc674118433629a07ac2
Ayo <alfonsoayo7@gmail.com>
2022-07-13 15:36:43 +0100

    Remove parse from check_in_artifact.start_time

--- app/controllers/api/check_in_templates_controller.rb
+++ app/controllers/api/check_in_templates_controller.rb
@@ -143,7 +143,7 @@ class Api::CheckInTemplatesController < Api::ApplicationController
   day_as_int = IceCube::RuleHelper.day_of_week_as_int(date_time_config)
   hour_as_int = IceCube::RuleHelper.hour_of_day_as_int(date_time_config)
   minute_as_int = IceCube::RuleHelper.minute_of_hour_as_int(date_time_config)
-  day = date_time_config["day"]
+   day = Time.parse(date_time_config["date"]).day
     begin
       case date_time_config["cadence"] 
         when "weekly"
@@ -275,7 +275,7 @@ class Api::CheckInTemplatesController < Api::ApplicationController
   minute_as_int = IceCube::RuleHelper.minute_of_hour_as_int(date_time_config)
 
   schedule = IceCube::Schedule.new(check_in_artifact.start_time)
-  day = date_time_config["day"]
+  day = Time.parse(date_time_config["date"]).day
   begin
     case date_time_config["cadence"] 
       when "weekly"
